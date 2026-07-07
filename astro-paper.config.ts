f92173966c9fbc4b5f8dba603f5d511509f08bbb
Jordan Thompson <jrdan.thompson@gmail.com>
2026-07-07 14:23:19 +1000

    fixing config info

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,14 +2,14 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    url: "https://jordanthompson.dev/",
+    title: "jordan thompson",
+    description: "This is my website. Stay a while. Or dont.",
+    author: "jordan thompson",
+    profile: undefined,
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "Australia/Brisbane",
     dir: "ltr",
   },
   posts: {
@@ -23,16 +23,15 @@ export default defineAstroPaperConfig({
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
-    },
+        enabled: true,
+        url: "https://github.com/jordan-at/domoblog/edit/main/",
+      },
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/jordan-at" },
+    { name: "linkedin", url: "https://www.linkedin.com/in/jordanalexthompson/" },
+    { name: "mail",     url: "mailto:jrdan.thompson@gmail.com" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
